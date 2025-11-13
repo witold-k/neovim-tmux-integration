da4d11cc5e091d51d5a2489246400a65727ca388
witold <witold@home>
2025-11-13 16:57:05 +0100

    extend config search path

--- exe/nide-config.lua
+++ exe/nide-config.lua
@@ -1,6 +1,37 @@
+local path
+local python_path = nil
+
+if python_path ~= nil then
+    path = '$HOME/svn/buildscripts/python'
+    if vim.loop.fs_stat(path) ~= nil then
+        python_path = path
+    end
+end
+
+if python_path ~= nil then
+    path = '$HOME/repos/buildscripts/python'
+    if vim.loop.fs_stat(path) ~= nil then
+        python_path = path
+    end
+end
+
+if python_path ~= nil then
+    path = '$HOME/repos/common-scripts/python'
+    if vim.loop.fs_stat(path) ~= nil then
+        python_path = path
+    end
+end
+
+if python_path ~= nil then
+    path = '$HOME/depots/common-scripts/python'
+    if vim.loop.fs_stat(path) ~= nil then
+        python_path = path
+    end
+end
+
 local config = {
     goproxy = 'https://goproxy.io,direct',
-    python_path = '$HOME/svn/buildscripts/python',
+    python_path = python_path,
 }
 
 local goproxy_env = 'GOPROXY=https://goproxy.io,direct'
